6249c58ddd46a6f59e07aeb70d8a7772fe0e61e6
Desperate Hiking <perevechenkov1999@gmail.com>
2019-10-21 15:38:04 +0500

    so vyrvyglazno

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -11,11 +11,11 @@ namespace ConsoleApp1
             int columns = mat.GetLength(1);
             double tmp, delta = 0.00001;
 
-            for (var columnIterations = 0; columnIterations < rows; columnIterations++)
+            for (var columnIteration = 0; columnIteration < rows; columnIteration++)
             {
-                for (var i = columnIterations; i < rows; i++)
+                for (var i = columnIteration; i < rows; i++)
                 {
-                    tmp = mat[i, columnIterations];
+                    tmp = mat[i, columnIteration];
                     if (Math.Abs(tmp) > delta)
                     {
                         for (var j = 0; j < columns; j++)
@@ -24,11 +24,11 @@ namespace ConsoleApp1
                         }
                     }
 
-                    if (i != columnIterations)
+                    if (i != columnIteration)
                     {
                         for (var j = 0; j < columns; j++)
                         {
-                            mat[i, j] -= mat[columnIterations, j];
+                            mat[i, j] -= mat[columnIteration, j];
                         }
                     }
                 }
@@ -36,21 +36,20 @@ namespace ConsoleApp1
             }
 
             var solution = new double[columns - 1];
+
             for (var i = 0; i < rows; i++)
             {
                 solution[i] = mat[i, columns - 1];
             }
+
             for (var i = rows - 2; i >= 0; i--)
             {
                 for (var j = i + 1; j < rows; j++)
                 {
                     solution[i] -= solution[j] * mat[i, j];
                 }
-            }
 
-            for (var i = 0; i < solution.Length; i++)
-            {
-                if (solution[i] < delta)
+                if (Math.Abs(solution[i]) < delta)
                     solution[i] = 0;
             }
 
@@ -66,10 +65,12 @@ namespace ConsoleApp1
             for (var i = 0; i < rows; i++)
             {
                 for (var j = 0; j < columns; j++)
-                {
+                {                   
                     if ((mat[i, j] % 1).ToString().Length > 3)
-                        Console.Write(Math.Round(mat[i, j], 2) + " ");
-                    else Console.Write(mat[i, j] + " ");
+                        mat[i, j] = Math.Round(mat[i, j], 2);
+                    if (j == 4)
+                        Console.Write(" = {0}", mat[i, j]);
+                    else Console.Write("X{0} * {1} ", j + 1, mat[i, j]);
                 }
                 Console.Write("\n");
             }
